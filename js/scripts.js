65e04949160314403fab752b70316e83cf04df43
llucaso <lredzinski@gmail.com>
2017-05-31 10:09:34 +0200

    get temp working

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,13 @@ $(function() {
 
 	function getTemp() {
 		var cityName = $('#city-name').val();
-		if (!cityName.length) cityname = 'Wrong City';
-
 
 		$.ajax({
-			url: url + cityName + '&APPID=9ca0cfe713c745eb66268a80dfa1ef98',
+			url: url + cityName + '&mode=json&units=metric' + '&APPID=9ca0cfe713c745eb66268a80dfa1ef98',
 			method: 'GET',
 			success: function(res) {
 				temp.text(res.main.temp);
+				$('#temp').append('*C');
 			}
 		});
 
